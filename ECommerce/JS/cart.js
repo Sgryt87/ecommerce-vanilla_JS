794980dab44589adaa4b11c50d562a294d0bc9f3
Sergii <sergiigrytsaienko@gmail.com>
2017-09-10 21:28:44 +0000

    fixed a rating, updated a cart

--- ECommerce/JS/cart.js
+++ ECommerce/JS/cart.js
@@ -1,28 +1,47 @@
 window.onload = function() {
   //  var spanQuantity = document.getElementById('product-name').innerHTML;
-   updateTotalQuantity();
-   updateCartList();
- }
- var myStorage = window.localStorage;
+  updateTotalQuantity();
+  updateCartList();
+  deleteItem();
+}
+var myStorage = window.localStorage;
 
- function updateTotalQuantity() {
-   var spanQuantity = document.getElementById('span-quantity');
-   var spanCount = 0;
-   for(var key in myStorage) {
-     spanCount +=  parseInt(myStorage.getItem(key));
-   }
-   spanQuantity.innerHTML = parseInt(spanCount);
- }
+function updateTotalQuantity() {
+  var spanQuantity = document.getElementById('span-quantity');
+  var spanCount = 0;
+  for (var key in myStorage) {
+    var obj = JSON.parse(myStorage.getItem(key));
+    spanCount += obj.quantity;
+  }
+  spanQuantity.innerHTML = parseInt(spanCount);
+}
 
- function updateCartList() {
-   var cartList = document.getElementById('cart-list');
-   var html = '';
-   for(var key in myStorage) {
-     html +=  '<div>'
-      + '<img src="../img/' + key + '.png">'
-      + '<p>' + key + '</p>'
-      +  
-      + '</div>';  //myStorage.getItem(key);
-   }
-   cartList.innerHTML = html;
- }
+function updateCartList() {
+  var cartList = document.getElementById('cart-list');
+  var html = '';
+  for (var key in myStorage) {
+    var obj = JSON.parse(myStorage.getItem(key));
+    html += '<div>' +
+      '<span class=\'deleteItemCart\' data-product="' + key + '">&times;</span>' +
+      '<img src="../img/' + key + '.png">' +
+      '<p>' + key + '</p>' +
+      '<p>' + '$' + (obj.quantity * obj.price).toFixed(2) + '</p>' +
+      '</div>'; //myStorage.getItem(key);
+  }
+  cartList.innerHTML = html;
+}
+
+function deleteItem() {
+  var deleteItemCart = document.getElementsByClassName('deleteItemCart');
+  console.log(deleteItemCart);
+  for (var i = 0; i < deleteItemCart.length; i++) {
+    deleteItemCart[i].addEventListener('click', function(e) {
+      console.log(e.target.dataset.product);
+      myStorage.removeItem(e.target.dataset.product);
+      window.location.reload();
+      // updateCartList();
+      // deleteItem();
+      // updateTotalQuantity();
+    });
+  }
+}
--- ECommerce/JS/ratingBar.js
+++ ECommerce/JS/ratingBar.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
-  var rating;
-	$('.rating-circle').hover(function () {
-		if (rating) {
-			rating.removeClass('rating-chosen');
-		}
-		$(this).prevAll().addBack().addClass('rating-hover');
-	});
-	$('.rating-circle').click(function () {
-		rating = $(this).prevAll().addBack();
-		rating.addClass('rating-chosen');
-	});
-	$('.rating-circle').mouseout(function () {
-		$(this).prevAll().addBack().removeClass('rating-hover');
-		if (rating) {
-			rating.addClass('rating-chosen');
-		}
-	});
+
+  for (var i = 0; i < 6; i++) {
+    ratingGenerator(i)
+  }
+  // END //
 });
+
+function ratingGenerator(i) {
+  var rating;
+  $('.rating-circle' + i).hover(function() {
+    if (rating) {
+      rating.removeClass('rating-chosen');
+    }
+    $(this).prevAll().addBack().addClass('rating-hover');
+  });
+  $('.rating-circle' + i).click(function() {
+    var myStorage = window.localStorage;
+    rating = $(this).prevAll().addBack();
+    rating.addClass('rating-chosen');
+    // myStorage.setItem(productName, JSON.stringify(dataObj));
+
+  });
+  $('.rating-circle' + i).mouseout(function() {
+    $(this).prevAll().addBack().removeClass('rating-hover');
+    if (rating) {
+      rating.addClass('rating-chosen');
+    }
+  });
+}
